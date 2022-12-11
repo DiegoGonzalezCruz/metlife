743d2664070fc888e0bbc30e2453009d93c78e9a
DiegoGonzalezCruz <diego@thinkey.cl>
2022-12-11 10:13:56 -0300

    almost done 90%

--- components/slider/SecondarySlider.tsx
+++ components/slider/SecondarySlider.tsx
@@ -30,7 +30,6 @@ export const SecondarySlider = () => {
               <div className="flex flex-col items-center justify-center gap-5 md:w-1/2 h-full py-5   ">
                 <div className="w-3/4 mx-auto flex flex-col gap-5 ">
                   <p className="text-white text-center">{feat.description}</p>
-
                   <Button className="font-Montserrat font-bold">
                     Llévame ahí
                   </Button>
